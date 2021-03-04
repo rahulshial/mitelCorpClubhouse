64b0649b601c54f0567b6e9087a9b055aaa49471
kozr <zxzflow@gmail.com>
2021-03-04 14:01:43 -0800

    firebase setup

---
+++ mitel-clubhouse/src/Debug.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import fireDb from '../../backend/utilities/firebase'
+
+const Debug = () => {
+    const [debugData, setDebugData] = useState({});
+    useEffect(() => {
+        const getDebugData = async () => {
+            setDebugData(await fireDb.getCollection('debugCollection'));
+        }
+        getDebugData();
+    }, []);
+
+    return (
+        <div className="debug">
+        <h1>{JSON.stringify(debugData)}</h1>
+        </div>
+    );
+}
+
+export default Debug;
