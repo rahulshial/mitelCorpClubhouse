ed82b5b1e91456cad060fe85d8d5b98b2681b455
Rahul Shial <rahul.shial@gmail.com>
2021-03-04 13:40:13 -0700

    initial commit

---
+++ mitel-clubhouse/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MITEL CORPORATE CLUBHOUSE</h1>
+    </div>
+  );
+}
+
+export default App;
