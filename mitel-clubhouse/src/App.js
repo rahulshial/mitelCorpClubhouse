498f3c1fc7b841f54c2443bf9b24b54bdca06d94
haohaodu <hdu2899@gmail.com>
2021-03-04 19:06:47 -0500

    Enable camera and audio permissions

--- mitel-clubhouse/src/App.js
+++ mitel-clubhouse/src/App.js
@@ -2,9 +2,38 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const options = {
+    video: true,
+    audio: true
+  }
+
+  const handleSuccess = (stream) => {
+    console.log('handle success');
+    const video = document.querySelector('video');
+    const videoTracks = stream.getVideoTracks();
+    console.log(`Using video device: ${videoTracks[0].label}`);
+    window.stream = stream; // make variable available to browser console
+    video.srcObject = stream;
+  }
+
+  const getUserMedia = async (constraints) => {
+      console.log('trying to click')
+      try
+      {
+        const stream = await navigator.mediaDevices.getUserMedia(options);
+        handleSuccess(stream);
+      }
+      catch (e) {
+        console.log('error: ', e);
+      }
+  }
+
   return (
     <div className="App">
       <h1>MITEL CORPORATE CLUBHOUSE</h1>
+      <button onClick={getUserMedia}>Click Me</button>
+      <video autoPlay playsInline></video>
     </div>
   );
 }
