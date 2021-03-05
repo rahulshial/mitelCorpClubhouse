0caab65b8256d13fca2762a942462249a170c5a8
Andrew Garneau <andrew.peter.garneau@gmail.com>
2021-03-04 19:03:46 -0500

    add router

--- mitel-clubhouse/src/App.js
+++ mitel-clubhouse/src/App.js
@@ -2,13 +2,31 @@ import React from 'react';
 import Debug from './Debug';
 import './App.css';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom"
+
+export default function App() {
   return (
-    <div className="App">
-      <Debug/>
-      <h1>MITEL CORPORATE CLUBHOUSE</h1>
-    </div>
+    <Router>
+      <div>
+        <h1>Main body to do (hallway and header)</h1>
+      </div>
+
+      <Switch>
+        <Route path="/user/:id">
+          <h2>User to do</h2>
+        </Route>
+        <Route path="/room/:id">
+          <h2>Room to do</h2>
+        </Route>
+      </Switch>
+
+    </Router>
   );
 }
 
-export default App;
