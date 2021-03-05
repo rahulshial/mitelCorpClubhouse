412cb3ef576003b0319b66df550ea3481fe0974f
kozr <zxzflow@gmail.com>
2021-03-05 08:30:02 -0800

    test 3

--- mitel-clubhouse/src/components/Debug.jsx
+++ mitel-clubhouse/src/components/Debug.jsx
@@ -5,10 +5,12 @@ import twilioSdk from '../utilities/twilio'
 const Debug = () => {
     const [debugData, setDebugData] = useState({});
     const [room, setRoom] = useState();
+    const [isMuted, setMute] = useState(false);
 
     useEffect(() => {
         const joinRoom = async (roomName) => {
-            const tempToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2Q0MzQ4ZTI4YmFjMWM4OGIzZjFjODJjZTQ5MjQ2NDNkLTE2MTQ5NTc1MTEiLCJpc3MiOiJTS2Q0MzQ4ZTI4YmFjMWM4OGIzZjFjODJjZTQ5MjQ2NDNkIiwic3ViIjoiQUM5MzA5NDViZTM0MmJjOGZmODFhZDg2YjE2OTAyM2YzMCIsImV4cCI6MTYxNDk2MTExMSwiZ3JhbnRzIjp7ImlkZW50aXR5IjoicGVyc29uMSIsInZpZGVvIjp7InJvb20iOiJtZWRpYS1yb29tIn19fQ.IWlh8I2ENfaKy-S1cBj6f81pe78gGP0z5hf2GDSOX2k'
+            const userName = 'test-user1'
+            const tempToken = twilioSdk.fetchVideoToken(userName, roomName)
             const currRoom = await twilioSdk.joinMediaRoom(tempToken, roomName)
             setRoom(currRoom)
             twilioSdk.subscribeToRoomMedia(currRoom, 'remote-audio')
@@ -24,6 +26,7 @@ const Debug = () => {
     return (
         <div className="debug">
         <audio id="remote-audio" autoPlay playsInline></audio>
+        {/* <button >{isMuted ? `click to mute` : `click to mute`}</button> */}
         <h1>{JSON.stringify(debugData)}</h1>
         <p>{JSON.stringify(room)}</p>
         </div>
--- mitel-clubhouse/src/utilities/twilio.js
+++ mitel-clubhouse/src/utilities/twilio.js
@@ -1,12 +1,51 @@
 import { connect } from 'twilio-video'
+import twilio from 'twilio'
 
 const twilioSdk = {
+    fetchVideoToken: (userName, roomName) => {
+        const twilioAccountSid = process.env.REACT_APP_TWILIO_SID;
+        const twilioApiKey = process.env.REACT_APP_TWILIO_API_KEY;
+        const twilioApiSecret = process.env.REACT_APP_TWILIO_API_SECRET;
+        
+        const identity = userName
+        
+        const AccessToken = twilio.jwt.AccessToken
+        
+        const videoGrant = new AccessToken.VideoGrant({
+            room: roomName
+        })
+        ;
+        
+        const token = new AccessToken(
+            twilioAccountSid,
+            twilioApiKey,
+            twilioApiSecret,
+            {identity: identity}
+        );
+        
+        token.addGrant(videoGrant);
+        
+        // Serialize the token to a JWT string
+        console.log(token.toJwt())
+        return token.toJwt()
+    },
+    muteLocalMedia: (room, shouldMute) => {
+        room.localParticipant.audioTracks.forEach(publication => {
+            if (shouldMute) publication.track.disable();
+            else publication.track.enable();
+          });
+    },
     subscribeToRoomMedia: (room, mediaDiv) => {
         room.participants.forEach(participant => {
             participant.tracks.forEach(publication => {
                 if (publication.track) {
                     document.getElementById(mediaDiv).appendChild(publication.track.attach());
                 }
+
+                // if (publication.isSubscribed) {
+                //     handleTrackDisabled(publication.track);
+                // }
+                // publication.on('subscribed', handleTrackDisabled);
             })
 
             participant.on('trackSubscribed', track => {
