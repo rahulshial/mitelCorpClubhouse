a134a01cce0f1cdc0c8542a1e610f531cc61b673
Andrew Garneau <andrew.peter.garneau@gmail.com>
2021-03-05 11:43:09 -0500

    add theme

--- mitel-clubhouse/src/AppTheme.js
+++ mitel-clubhouse/src/AppTheme.js
@@ -11,5 +11,27 @@ export const appTheme = createMuiTheme({
         background: {
             default: "rgba(30, 146, 223, 0.3)",
         }
+    },
+    typography: {
+        h1: { // individual room title, 
+            fontSize: "36px",
+            fontFamily: "Quicksand",
+            fontWeight: "700",
+        },
+        h2: { // hallway room title
+            fontSize: "30px",
+            fontFamily: "Quicksand",
+            fontWeight: "700"
+        },
+        body1: { // names
+            fontSize: "16px",
+            fontFamily: "Quicksand",
+            fontWeight: "500"
+        },
+        button: {
+            fontSize: "24px",
+            fontFamily: "Quicksand",
+            fontWeight: "600"
+        }
     }
 });
